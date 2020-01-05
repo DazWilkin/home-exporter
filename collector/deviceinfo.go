d72131ca4c354f3844f99368e96c49e9dd2fdc7d
Daz Wilkin <daz.wilkin@gmail.com>
2020-01-05 12:01:24 -0800

    Added: Up; BuildInfo

--- collector/deviceinfo.go
+++ collector/deviceinfo.go
@@ -12,6 +12,8 @@ import (
 type DeviceInfoCollector struct {
 	client *gohome.Client
 
+	Up          *prometheus.Desc
+	BuildInfo   *prometheus.Desc
 	NoiseLevel  *prometheus.Desc
 	SignalLevel *prometheus.Desc
 	Uptime      *prometheus.Desc
@@ -19,6 +21,7 @@ type DeviceInfoCollector struct {
 
 // NewDeviceInfoCollector returns a new DeviceInfoCollector.
 func NewDeviceInfoCollector(client *gohome.Client) *DeviceInfoCollector {
+	subsystem := "device_info"
 	labelNames := []string{
 		"name",
 		"build_version",
@@ -26,21 +29,33 @@ func NewDeviceInfoCollector(client *gohome.Client) *DeviceInfoCollector {
 	}
 	return &DeviceInfoCollector{
 		client: client,
+		Up: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "up"),
+			"If 1 the Home device is accessible, 0 otherwise",
+			labelNames,
+			nil,
+		),
+		BuildInfo: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "build_info"),
+			"A metric with a constant '1' value labeled by X of the device",
+			[]string{"build_version", "cast_build_revision", "release_track"},
+			nil,
+		),
 		NoiseLevel: prometheus.NewDesc(
-			prometheus.BuildFQName("home", "device_info", "noise_level"),
-			"TBD",
+			prometheus.BuildFQName(namespace, subsystem, "noise_level"),
+			"Noise Level dB",
 			labelNames,
 			nil,
 		),
 		SignalLevel: prometheus.NewDesc(
-			prometheus.BuildFQName("home", "device_info", "signal_level"),
-			"TBD",
+			prometheus.BuildFQName(namespace, subsystem, "signal_level"),
+			"Signal Level dB",
 			labelNames,
 			nil,
 		),
 		Uptime: prometheus.NewDesc(
-			prometheus.BuildFQName("home", "device_info", "uptime"),
-			"TBD",
+			prometheus.BuildFQName(namespace, subsystem, "uptime"),
+			"Uptime in seconds",
 			labelNames,
 			nil,
 		),
@@ -54,6 +69,11 @@ func (c *DeviceInfoCollector) Collect(ch chan<- prometheus.Metric) {
 		log.Println(err)
 		return
 	}
+	ch <- prometheus.MustNewConstMetric(
+		c.BuildInfo,
+		prometheus.CounterValue,
+		1.0,
+		info.BuildVersion, info.CastBuildRevision, info.ReleaseTrack)
 	labelValues := []string{
 		info.Name,
 		info.BuildVersion,
